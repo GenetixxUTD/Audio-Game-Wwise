617ee6eff855ecb5b30d9e724eaadd55ba5c1971
GenetixxUTD <cameronshirley8@googlemail.com>
2024-04-12 02:15:23 +0100

    Shotgun Event Created and Wired

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_PLAYERPISTOLSHOT = 3923112352U;
+        static const AkUniqueID PLAY_PLAYSHOTGUNSHOT = 2302239150U;
     } // namespace EVENTS
 
     namespace BANKS
